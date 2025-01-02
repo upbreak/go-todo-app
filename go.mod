b59357713925d6acb3dc476396e157aabd328bea
kimjinwoo <upbreak05@naver.com>
2025-01-02 17:53:00 +0900

    http 서버 실행 리팩토링

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/upbreak/go-todo-app
 
 go 1.23.4
+
+require golang.org/x/sync v0.10.0 // go get -u golang.org/x/sync: main()에서 사용할 run함수를 구현할 때 이용하는 준표준 패키지
--- main.go
+++ main.go
@@ -3,17 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
+	//"os"
+	"context"
+	"log"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello World")
-		}))
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v\n", err)
-		os.Exit(1)
+	if err:= run(context.Background()) ; err != nil {
+		log.Printf("failed to terminate server: %v", err)
 	}
+}
+
+func run(ctx context.Context) error {
+	server := &http.Server{
+		Addr:    ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+			fmt.Fprintf(w, "Hello World! %s", r.URL.Path[1:])
+		}),
+	}
+
+	// 다른 고루틴에서 http서버를 실행
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close %v", err);
+			return err
+		}
+		return nil
+	})
+
+	//채널로부터 알림(종료)을 기다린다
+	<-ctx.Done()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("failed to shutdown server: %v", err)
+	}
+
+	// Go 메서드로 실행한 다른 고루팅의 종료를 기다림
+	return eg.Wait()
+
 }
