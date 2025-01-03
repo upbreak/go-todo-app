f2a7da9c15b6d3a8fcc0190365605ac39a029f41
kimjinwoo <upbreak05@naver.com>
2025-01-03 16:26:27 +0900

    task 라우팅 설정
    http메서드에 따라 핸들러를 구현하기 어렵기 때문에 "github.com/go-chi/chi/v5"를 사용하여 라우팅을 설정

--- mux.go
+++ mux.go
@@ -1,12 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator"
+	"github.com/upbreak/go-todo-app/handler"
+	"github.com/upbreak/go-todo-app/store"
+	"net/http"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
+
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+
+	v := validator.New()
+	mux.Handle("/tasks", &handler.AddTask{Store: store.Tasks, Validator: v})
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
+
 	return mux
 }
