6fa0920c6e8bd36796a8f9550fea9c40141454bf
kimjinwoo <upbreak05@naver.com>
2025-01-03 10:32:03 +0900

    환경변수로부터 값을 가져와 서버 실행
    - github.com/caarlos0/env를 사용하여 struct에 태그를 붙여 환경변수로부터 데이터를 가져올수 있게 함.
    - config.go의 struct를 이용하여 main.go의 run함수가 환경변수를 불러와 서버를 실행할 수 있도록 수정
    - run()함수에서 net.listen을 사용하여 동적으로 포트를 불러오도록 수정

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/upbreak/go-todo-app
 go 1.23.4
 
 require golang.org/x/sync v0.10.0 // go get -u golang.org/x/sync: main()에서 사용할 run함수를 구현할 때 이용하는 준표준 패키지
+
+require github.com/caarlos0/env v3.5.0+incompatible // go get -u github.com/caarlos0/env: struct에 태그를 사용하여 환경변수를 가져와 사용할 수 있게 해준다
--- main.go
+++ main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	//"os"
 	"context"
-	"log"
+	"fmt"
+	"github.com/upbreak/go-todo-app/config"
 	"golang.org/x/sync/errgroup"
+	"log"
+	"net"
+	"net/http"
 )
 
 func main() {
-	if err:= run(context.Background()) ; err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 	}
 }
 
 func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+
 	server := &http.Server{
-		Addr:    ":18080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		//Addr:    ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello World! %s", r.URL.Path[1:])
 		}),
 	}
@@ -26,8 +40,8 @@ func run(ctx context.Context) error {
 	// 다른 고루틴에서 http서버를 실행
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close %v", err);
+		if err := server.Serve(l); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close %+v", err)
 			return err
 		}
 		return nil
